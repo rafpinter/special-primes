480612300642b86028f5dc592ec3aded2b8e356f
Rafaela Pinter <rpinter811@gmail.com>
2024-03-08 13:37:58 -0500

    adding previous tests that I forgot to register

---
+++ test/test.h
@@ -0,0 +1,14 @@
+//
+// Created by Rafaela Pinter on 2024-03-08.
+//
+
+#ifndef Q2_NOMBRES_PREMIERS_EN_SEQUENCE_TEST_H
+#define Q2_NOMBRES_PREMIERS_EN_SEQUENCE_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //Q2_NOMBRES_PREMIERS_EN_SEQUENCE_TEST_H
